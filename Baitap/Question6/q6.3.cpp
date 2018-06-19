0e7d628688890ac6d9b7e8dedb946a64aec54425
DongTan1102 <dong.tan.11296@gmail.com>
2018-06-19 17:13:30 +0700

    Finish Question6

---
+++ Baitap/Question6/q6.3.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+
+using namespace std;
+
+
+void copyString(char *input, int offset, int length, bool invert, char *output, int output_offset)
+{
+	char tam[100];
+	char *tmp = new char[length];
+	for (int i = 0; i < length; i++)
+	{
+		tmp[i] = input[offset];
+		offset++;
+		cout << tmp << endl;
+	}
+
+	cout << tmp << endl;
+
+	if (invert)
+	{
+		for (int i = 0; i <length / 2; i++)
+		{
+			swap(tmp[i], tmp[length - i - 1]);
+		}
+	}
+
+	cout << tmp << endl;
+
+//	memcpy(&output[output_offset], tmp, sizeof(tmp) + 1);
+	strcpy(&output[output_offset], tmp);
+
+	delete[] tmp;
+}
+
+
+void main() {
+	
+	char input[] = "Hello world";
+	char output[100] = "My name is C++";
+
+	cout << input << endl;
+	cout << output << endl;
+
+	copyString(input, 6, 5, true, output, 11);
+
+	cout << input << endl;
+	cout << output << endl;
+
+	system("pause");
+}
