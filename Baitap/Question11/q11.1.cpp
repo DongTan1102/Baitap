3fc69015576d05327e7c66ee704939cccb7d4506
DongTan1102 <dong.tan.11296@gmail.com>
2018-06-28 17:18:07 +0700

    Xong ngay 28-6-2018

---
+++ Baitap/Question11/q11.1.cpp
@@ -0,0 +1,39 @@
+//#include <iostream>
+//#include <istream>
+//#include <cstdio>
+//#include<iomanip>
+//#include <string>
+//#include <fstream>
+//
+//using namespace std;
+//
+//template <class R, class T> 
+//
+//R Average(int count, T* list)
+//{
+//	float S, avg;
+//	for (int i = 0; i < count; i++)
+//	{
+//		S += list[i];
+//	}
+//
+//	avg = S / count;
+//
+//	return avg;
+//}
+//
+//int main() {
+//	
+//	int list[100];
+//	int n;
+//
+//	cout << "Nhap so phan tu cua mang: n = ";
+//	cin >> n;
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		cout << "\nlist[" << i << "] = ";
+//	}
+//
+////	Average(n, list);
+//}
